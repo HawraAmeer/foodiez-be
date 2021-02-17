07cde754f62a21994e0d4e9e91e399bdfb406d0c
HawraAmeer <hwra.ameer97@gmail.com>
2021-02-17 16:12:59 +0300

    Recipe is done

---
+++ controllers/recipeCtrl.js
@@ -0,0 +1,41 @@
+const { Recipe } = require("../db/models");
+
+exports.fetchRecipe = async (recipeId, next) => {
+  try {
+    return await Recipe.findByPk(recipeId);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.createRecipe = async (req, res, next) => {
+  try {
+    if (req.file) {
+      req.body.image = `http://${req.get("host")}/media/${req.file.filename}`;
+    }
+    const newRecipe = await Recipe.create(req.body);
+    res.status(201).json(newRecipe);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.recipeList = async (req, res, next) => {
+  try {
+    const recipes = await Recipe.findAll({
+      attributes: { exclude: ["createdAt", "updatedAt"] },
+      //   include: {
+      //     model: Category,
+      //     as: "category",
+      //     attributes: ["id"],
+      //   },
+    });
+    res.json(recipes);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.recipeDetail = async (req, res, next) => {
+  res.json(req.recipe);
+};
--- routes/recipes.js
+++ routes/recipes.js
@@ -1,5 +1,20 @@
 const express = require("express");
+const recipeCtrl = require("../controllers/recipeCtrl");
 const router = express.Router();
 const upload = require("../middlewares/multer");
+
+router.param("recipeId", async (req, res, next, recipeId) => {
+  const foundRecipe = await recipeCtrl.fetchRecipe(recipeId, next);
+  if (foundRecipe) {
+    req.recipe = foundRecipe;
+    next();
+  } else next({ status: 404, message: "Recipe Not Found." });
+});
+
+router.post("/", upload.single("image"), createRecipe);
+
+router.get("/", recipeCtrl.recipeList);
+
+router.get("/:recipeId", recipeCtrl.recipeDetail);
 
 module.exports = router;
