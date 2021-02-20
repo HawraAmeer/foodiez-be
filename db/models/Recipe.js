c336ea19650e251a0faacf42c8ab4b2cdc26bcac
JannatJameel <j_annat@outlook.com>
2021-02-20 22:00:37 +0300

    added defautl values

--- db/models/Recipe.js
+++ db/models/Recipe.js
@@ -4,8 +4,16 @@ module.exports = (sequelize, DataTypes) => {
   const Recipe = sequelize.define("Recipe", {
     name: { type: DataTypes.STRING, allowNull: false },
     slug: { type: DataTypes.STRING, unique: true },
-    image: { type: DataTypes.STRING, allowNull: true },
-    method: { type: DataTypes.STRING, allowNull: true },
+    image: { 
+      type: DataTypes.STRING, 
+      allowNull: true, 
+      defaultValue: "https://i.pinimg.com/originals/8e/fe/ea/8efeea6762f92ecb701cc0652cde2331.jpg" 
+    },
+    method: { 
+      type: DataTypes.STRING, 
+      allowNull: true, 
+      defaultValue: "Enter the recipe steps"
+    },
   });
 
   SequelizeSlugify.slugifyModel(Recipe, {
